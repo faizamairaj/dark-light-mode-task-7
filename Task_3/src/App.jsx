b26d91505996a7d297ae42e10f61d4179dd0205e
faizamairaj <faizamairaj0@gmail.com>
2025-01-21 12:26:30 +0500

    dark/light-mode task-7

--- Task_3/src/App.jsx
+++ Task_3/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App()  {
+function App() {
+  const [mode, setMode] = useState(false);
 
-  const [mode , setMode] = useState (false);
-
-    return (
-        <>
-        <h1 className='text-5xl text-white font-bold'>Light/Dark Mode</h1>
-            <button className='text-3xl rounded-md font-semibold p-3 mt-72 ' style={{backgroundColor : mode ? "White" : "Black",
-            color : mode ? "Black" : "White",
-            }}onClick={() =>{setMode (!mode)}}>{mode ? "Dark" : "Light"}Mode</button>
-        </>
-      
-    );
+  return (
+    <>
+  <i><h1 className='text-5xl font-bold text-center mt-8 text-white'>
+        Light/Dark Mode
+      </h1></i>
+      <button
+        className='text-3xl rounded-md font-semibold p-3 mt-72 mx-auto block sm:mt-36 sm:text-xl'
+        style={{
+          backgroundColor: mode ? 'White' : 'Black',
+          color: mode ? 'Black' : 'White',
+        }}
+        onClick={() => { setMode(!mode); }}
+      >
+        {mode ? 'Dark' : 'Light'} Mode
+      </button>
+    </>
+  );
 }
 
 export default App;
